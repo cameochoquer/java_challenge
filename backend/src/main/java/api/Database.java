336686360f6428ae8ab34beebad46f4068a7b6e7
cameochoquer <cjchoquer@gmail.com>
2025-06-18 21:11:50 +0100

    fix: clean up files with linter/prettier

--- backend/src/main/java/api/Database.java
+++ backend/src/main/java/api/Database.java
@@ -1,41 +1,35 @@
 package api;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-    public class Database
-    {
-      public static void main(String[] args)
-      {
-        // NOTE: Connection and Statement are AutoCloseable.
-        //       Don't forget to close them both in order to avoid leaks.
-        try
-        (
-          // create a database connection
-          Connection connection = DriverManager.getConnection("jdbc:sqlite:tasks.db");
-          Statement statement = connection.createStatement();
-        )
-        {
+public class Database {
+  public static void main(String[] args) {
+    // NOTE: Connection and Statement are AutoCloseable.
+    // Don't forget to close them both in order to avoid leaks.
+    try (
+        // create a database connection
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:tasks.db");
+        Statement statement = connection.createStatement();) {
 
-          statement.setQueryTimeout(30);  // set timeout to 30 sec.
-          statement.executeUpdate("drop table if exists tasks");
-          statement.executeUpdate("create table tasks (id integer, title string, description string, status string, due_date_time string)");
-          statement.executeUpdate("insert into tasks values(1, 'clean dishes', 'with soap and water', 'to do','' )");
-          ResultSet rs = statement.executeQuery("select * from tasks");
-          while(rs.next())
-          {
-            // read the result set
-            System.out.println("name = " + rs.getString("title"));
-            System.out.println("id = " + rs.getInt("status"));
-          }
-        }
-        catch(SQLException e)
-        {
-          // if the error message is "out of memory",
-          // it probably means no database file is found
-          e.printStackTrace(System.err);
-        }
+      statement.setQueryTimeout(30); // set timeout to 30 sec.
+      statement.executeUpdate("drop table if exists tasks");
+      statement.executeUpdate(
+          "create table tasks (id integer, title string, description string, status string, due_date_time string)");
+      statement.executeUpdate("insert into tasks values(1, 'clean dishes', 'with soap and water', 'to do','' )");
+      ResultSet rs = statement.executeQuery("select * from tasks");
+      while (rs.next()) {
+        // read the result set
+        System.out.println("name = " + rs.getString("title"));
+        System.out.println("id = " + rs.getInt("status"));
       }
+    } catch (SQLException e) {
+      // if the error message is "out of memory",
+      // it probably means no database file is found
+      e.printStackTrace(System.err);
     }
+  }
+}
