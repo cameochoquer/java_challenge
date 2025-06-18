99b2782ad9257bb3c49550ed72555e78c83115d8
cameochoquer <cjchoquer@gmail.com>
2025-06-18 20:50:40 +0100

    changes to the sqlite table

--- backend/src/main/java/api/Database.java
+++ backend/src/main/java/api/Database.java
@@ -14,22 +14,21 @@ import java.sql.Statement;
         try
         (
           // create a database connection
-          Connection connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+          Connection connection = DriverManager.getConnection("jdbc:sqlite:tasks.db");
           Statement statement = connection.createStatement();
         )
         {
-          statement.setQueryTimeout(30);  // set timeout to 30 sec.
 
-          statement.executeUpdate("drop table if exists person");
-          statement.executeUpdate("create table person (id integer, name string)");
-          statement.executeUpdate("insert into person values(1, 'leo')");
-          statement.executeUpdate("insert into person values(2, 'yui')");
-          ResultSet rs = statement.executeQuery("select * from person");
+          statement.setQueryTimeout(30);  // set timeout to 30 sec.
+          statement.executeUpdate("drop table if exists tasks");
+          statement.executeUpdate("create table tasks (id integer, title string, description string, status string, due_date_time string)");
+          statement.executeUpdate("insert into tasks values(1, 'clean dishes', 'with soap and water', 'to do','' )");
+          ResultSet rs = statement.executeQuery("select * from tasks");
           while(rs.next())
           {
             // read the result set
-            System.out.println("name = " + rs.getString("name"));
-            System.out.println("id = " + rs.getInt("id"));
+            System.out.println("name = " + rs.getString("title"));
+            System.out.println("id = " + rs.getInt("status"));
           }
         }
         catch(SQLException e)
